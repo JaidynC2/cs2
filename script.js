37d1e8a877e923eb57f1ead6b71602e373dd6739
JC CLARK <>
2023-03-02 14:21:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let optionOneScreen = document.querySelector("one");
+let optionOneButton = document.querySelector("option-one");
+
+
+
+
+
+
+
+
+
+
+
+optionOneButton.onclick = function(){
+  optionOneScreen.style.display = "block";
+  openingScreen.style.display - "no";
+}
